ff89492373d9819535d8fe3e0a8212ab4c591f97
Miguel Mateo tolino <mmateo-t@c1r11s1.42madrid.com>
2020-02-09 00:56:18 +0100

    next update

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM debian:buster
+MAINTAINER mmateo-t <mmateo-t@42.student.com>
 
 RUN apt update && \
 	apt -y upgrade && \
@@ -10,17 +11,20 @@ RUN apt update && \
 
 COPY	srcs/wordpress /var/www/wordpress/
 COPY	srcs/phpMyAdmin-4.9.4-all-languages/* /var/www/phpMyAdmin/
+COPY	srcs/config.inc.php	/var/www/phpMyAdmin/
 COPY	srcs/default  /etc/nginx/sites-avalibles/
-COPY	srcs/config.sql  /home/
+COPY	srcs/config.sql  /tmp/
+COPY	srcs/wordpress.sql /tmp/
 
-#RUN	chown -R www-data:www-data /var/www/* && \
-#	chmod -R 755 /var/www/* 
+RUN	chown -R www-data:www-data /var/www/* && \
+	chmod -R 755 /var/www/*
+#	ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
 RUN		service mysql start && \
-		mysql -u root --password= < /home/config.sql
+		mysql -u root --password= < /tmp/config.sql && \
+		mysql wordpress -u root --password= < /tmp/wordpress.sql
 
-EXPOSE 80
 
 CMD service nginx start && \
 	service mysql start && \
-	service php-fpm start && \
+	service php7.3-fpm start && \
 	bash
--- srcs/wordpress/wp-config.php
+++ srcs/wordpress/wp-config.php
@@ -42,14 +42,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY', 'pon aquí tu frase aleatoria'); // Cambia esto por tu frase aleatoria.
-define('SECURE_AUTH_KEY', 'pon aquí tu frase aleatoria'); // Cambia esto por tu frase aleatoria.
-define('LOGGED_IN_KEY', 'pon aquí tu frase aleatoria'); // Cambia esto por tu frase aleatoria.
-define('NONCE_KEY', 'pon aquí tu frase aleatoria'); // Cambia esto por tu frase aleatoria.
-define('AUTH_SALT', 'pon aquí tu frase aleatoria'); // Cambia esto por tu frase aleatoria.
-define('SECURE_AUTH_SALT', 'pon aquí tu frase aleatoria'); // Cambia esto por tu frase aleatoria.
-define('LOGGED_IN_SALT', 'pon aquí tu frase aleatoria'); // Cambia esto por tu frase aleatoria.
-define('NONCE_SALT', 'pon aquí tu frase aleatoria'); // Cambia esto por tu frase aleatoria.
+define('AUTH_KEY',         'j[Yq9<`u5Q8Ljz-:~2dECV?pwVpAL_v-|-|<}[u^-e7tj,&N{fS>9~M?]viD8OX-');
+define('SECURE_AUTH_KEY',  '|CRWGBH;*9V<HeV7lF|D-2{~n&Wp=j(_cp;dl4!$GaO<|+LcWlbW{vU14.9ZD$,O');
+define('LOGGED_IN_KEY',    '18e<s6x.YV3>M@C-M@2]Dgk&pLmP`<8FnBY#AqTQ1e:>jubHey`E}oQvN#?zh{H>');
+define('NONCE_KEY',        '3[J.o)DS:ZL7@|kJ(=bk=}/P${h9RI9j]DcB{pfQqSL5?=hlG~~{>`-r#Q/N:+wc');
+define('AUTH_SALT',        'x7htpo/|E+qLY[#< vGcZJ#@xjvH<{3x08O|{4X*WyT3(7-qCdt<yS~Sb1/F,%-Y');
+define('SECURE_AUTH_SALT', 'm.4iaNW!!ppqtlJRpyB5:3-p-4;%@3y`q-J)+!e:,&|<T)-qbe%Dj$i6qO.SB+(-');
+define('LOGGED_IN_SALT',   '/;}q|cn~:}~/X*gef=7g?0M3[w?]X3&we+feDl-+-Hk=E_im/Cf;?EK,]g2D*o-C');
+define('NONCE_SALT',       'Foz0J2Flt.hS!Tq?g]ZyoA,88GM4QRr`h+Ja?au*HAP(S:t1/Kb^bxVl:^|3%n.g');
 
 /**#@-*/
 
